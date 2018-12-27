8115ee627862dc6c5039a954385d7e40d8b9c9ab
CrescendoFang <crescendofang@gmail.com>
2018-12-27 12:31:37 +0700

    MIUI 10 (Oreo)

--- config.sh
+++ config.sh
@@ -53,18 +53,11 @@ print_modname() {
 # Check the documentations for more info about how Magic Mount works, and why you need this
 
 REPLACE="
-/system/bin/busybox
 /system/etc/device_features/natrium.xml
-/system/etc/gps.conf
-/system/etc/izat.conf
-/system/etc/lowi.conf
-/system/etc/xtwifi.conf
 /system/fonts/NotoColorEmoji.ttf
 /system/media/audio/ui/camera_click.ogg
 /system/media/audio/ui/camera_focus.ogg
 /system/usr/keylayout/synaptics_dsx.kl
-/system/vendor/bin/xtra-daemon
-/system/vendor/lib64/libbt-vendor.so
 "
 
 ##########################################################################################
@@ -76,8 +69,6 @@ set_permissions() {
   # The default permissions should be good enough for most cases
 
   set_perm_recursive  $MODPATH  0  0  0755  0644
-  set_perm	$MODPATH/system/bin/busybox				0	0	0755
-  set_perm	$MODPATH/system/vendor/bin/xtra-daemon	0	0	0755
 }
 
 ##########################################################################################
